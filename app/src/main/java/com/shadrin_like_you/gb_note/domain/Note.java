67dc738d9688c6788face4410e6536c5fcd34b5a
Like_you <staller.home163@gmail.com>
2022-04-27 19:03:02 +0400

    Lesson7 ДЗ по методичке не очень понял что от меня хотят, если потом все равно переделывать... Из того что понял, будет фрагмент с меню и кнопкой отдельным окном как на уроке. Сюда же можно впихнуть подменю (как раз сделать child фрагмент) ну и дальше наводить красоту...

--- app/src/main/java/com/shadrin_like_you/gb_note/domain/Note.java
+++ app/src/main/java/com/shadrin_like_you/gb_note/domain/Note.java
@@ -6,11 +6,24 @@ import android.os.Parcelable;
 import java.util.Date;
 
 public class Note implements Parcelable {
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+
+            return new Note[size];
+        }
+    };
+    int icon;                       //иконка
     private String title;           //заголовок
     private String content;         //содержимое
     private Date createDate;        //дата и время создания
     private boolean state;          //состояние (отметка о выполнении)
-    int icon;                       //иконка
 
     public Note(String title, String content, Date createDate, boolean state, int icon) {
         this.title = title;
@@ -21,7 +34,7 @@ public class Note implements Parcelable {
 
     }
 
-    public Note(String string) {
+    public Note(String string, int ic_baseline_auto_stories_24) {
     }
 
     protected Note(Parcel in) {
@@ -31,48 +44,36 @@ public class Note implements Parcelable {
         icon = in.readInt();
     }
 
-    public static final Creator<Note> CREATOR = new Creator<Note>() {
-        @Override
-        public Note createFromParcel(Parcel in) {
-            return new Note(in);
-        }
-
-        @Override
-        public Note[] newArray(int size) {
-            return new Note[size];
-        }
-    };
+    public String getTitle() {
+        return title;
+    }
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public void setState(boolean state) {
-        this.state = state;
-    }
-
-    public String getTitle() {
-        return title;
     }
 
     public String getContent() {
         return content;
     }
 
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     public Date getCreateDate() {
         return createDate;
     }
 
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
     public boolean isState() {
         return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
     }
 
     public int getIcon() {
