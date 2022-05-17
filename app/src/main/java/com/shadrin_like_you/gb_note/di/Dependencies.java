ecc6442d28f47d6394b08bb287da257a56a9efad
Like_you <staller.home163@gmail.com>
2022-05-17 23:40:03 +0400

    Lesson 12
    Step 1

--- app/src/main/java/com/shadrin_like_you/gb_note/di/Dependencies.java
+++ app/src/main/java/com/shadrin_like_you/gb_note/di/Dependencies.java
@@ -1,11 +1,12 @@
 package com.shadrin_like_you.gb_note.di;
 
+import com.shadrin_like_you.gb_note.domain.FireStoreNotesRepo;
 import com.shadrin_like_you.gb_note.domain.InMemoryNotesRepo;
 import com.shadrin_like_you.gb_note.domain.NotesRepository;
 
 public class Dependencies {
 
-    public static final NotesRepository NOTES_REPOSITORY = new InMemoryNotesRepo();
+    public static final NotesRepository NOTES_REPOSITORY = new FireStoreNotesRepo();
 
 
     public static NotesRepository getNotesRepository() {
